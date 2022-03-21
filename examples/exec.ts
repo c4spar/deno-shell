4beabef983178f6e9c547a337edf104a55fe8cd3
Benjamin Fischer <c4spar@gmx.de>
2022-03-21 13:07:41 +0100

    some refactorings

--- examples/exec.ts
+++ examples/exec.ts
@@ -4,14 +4,13 @@ import { exec, execSync } from "../src/ffi.ts";
 log("$ pwd");
 log("Exit code:", await exec("pwd"));
 
-log("Change cwd to: src");
-Deno.chdir("src");
+log("$ pwd");
+log("Exit code:", execSync("pwd", { cwd: "examples" }));
 
+log("Change dir: src");
+Deno.chdir("src");
 log("$ pwd");
 log("Exit code:", await exec("pwd"));
-
-log("$ echo foo bar baz");
-log("Exit code:", execSync("echo foo bar baz"));
 
 function log(msg: string, ...args: Array<unknown>) {
   console.log(blue(msg), ...args);
--- src/ffi.ts
+++ src/ffi.ts
@@ -1,3 +1,5 @@
+import { red } from "../deps.ts";
+
 enum LibSuffix {
   windows = "dll",
   darwin = "dylib",
@@ -11,24 +13,62 @@ const libName = `./target/debug/libdeno_shell.${libSuffix}`;
 const dylib = Deno.dlopen(libName, {
   "deno_shell_exec": {
     name: "deno_shell_exec_sync",
-    parameters: ["pointer"],
-    result: "i32",
+    parameters: ["pointer", "usize"],
+    result: "pointer",
     nonblocking: true,
   },
   "deno_shell_exec_sync": {
-    parameters: ["pointer"],
-    result: "i32",
+    parameters: ["pointer", "usize"],
+    result: "pointer",
   },
 });
 
-export function exec(script: string): Promise<number> {
-  return dylib.symbols.deno_shell_exec(encode(script));
+interface ExecOptions {
+  cwd?: string;
 }
 
-export function execSync(script: string): number {
-  return dylib.symbols.deno_shell_exec_sync(encode(script));
+interface ExecResult {
+  exit_code: number;
+  error_message?: string;
 }
 
-function encode(value: string): Uint8Array {
-  return encoder.encode(value + "\0");
+export async function exec(
+  script: string,
+  options: ExecOptions = {},
+): Promise<number> {
+  const pointer = await dylib.symbols.deno_shell_exec(
+    ...encodeJson({
+      cwd: Deno.cwd(),
+      ...options,
+      script,
+    }),
+  );
+  return parseExecResult(pointer);
+}
+
+export function execSync(script: string, options: ExecOptions = {}): number {
+  const pointer = dylib.symbols.deno_shell_exec_sync(
+    ...encodeJson({
+      cwd: Deno.cwd(),
+      ...options,
+      script,
+    }),
+  );
+  return parseExecResult(pointer);
+}
+
+function encodeJson(val: unknown): [Uint8Array, number] {
+  const objectStr = JSON.stringify(val);
+  const buf = encoder.encode(objectStr);
+  return [buf, buf.length];
+}
+
+function parseExecResult(pointer: Deno.UnsafePointer): number {
+  const view = new Deno.UnsafePointerView(pointer);
+  const cStr = view.getCString();
+  const result: ExecResult = JSON.parse(cStr);
+  if (result.error_message) {
+    throw new Error(red(result.error_message));
+  }
+  return result.exit_code;
 }
